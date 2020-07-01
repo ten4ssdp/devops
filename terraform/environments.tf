9b94ac14566dd02542791a9a3e6cc1b89ebf9961
Kevin <kevin.manssat@hotmail.fr>
2020-07-01 20:56:04 +0200

    add second instance

--- terraform/environments.tf
+++ terraform/environments.tf
@@ -3,7 +3,7 @@ module "production" {
 
   instance_type     = "t2.micro"
   instance_ami      = "${data.aws_ami.ubuntu.id}"
-  instance_count    = 1
+  instance_count    = 2
   instance_key_name = "${aws_key_pair.adminkeypair.key_name}"
   stage             = "production"
 }
